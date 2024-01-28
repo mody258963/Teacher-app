0cc4b5af20484c195a90221533abfb41991ac24d
mody258963 <122531523+mody258963@users.noreply.github.com>
2024-01-28 13:19:29 +0200

    last

--- teacher/lib/presntation_lyar/screens/CourseScreen.dart
+++ teacher/lib/presntation_lyar/screens/CourseScreen.dart
@@ -26,19 +26,26 @@ class _CourseScreenState extends State<CourseScreen> {
   }
 
   Widget _buildGetAllDataSumbit(BuildContext context) {
+  double width = MediaQuery.of(context).size.width;
+  double height = MediaQuery.of(context).size.height;
     return BlocBuilder<GetMethodCubit, GetMethodState>(
         builder: (context, state) {
       if (state is CourseOfTeacherState) {
         final allUsersList = state.posts;
         return SizedBox(
-          height: 500,
+          height: height * 0.767,
+          width:  width * 0.95,
           child: ListView.builder(
               itemCount: allUsersList.length,
               itemBuilder: (context, index) {
                 final user = allUsersList[index];
-                return ListTile(
-                  title: Text(user.title.toString()),
-                  subtitle: Text(user.description.toString()),
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(7),color: Colors.yellowAccent),
+                    height: height * 0.20,
+                    child: Text(user.title.toString()),
+                  ),
                 );
               }),
         );
@@ -47,9 +54,27 @@ class _CourseScreenState extends State<CourseScreen> {
     });
   }
 
-  Widget _Gester() {
-    return GestureDetector();
-  }
+  // Widget _Gester(title) {
+  //   double width = MediaQuery.of(context).size.width;
+  //   double height = MediaQuery.of(context).size.height;
+  //   return Container(
+  //     color: Colors.blueAccent,
+  //     height: height * 0.30,
+  //     width: width * 0.80,
+  //     child: Column(
+  //       children: [Text(title),
+  //       Text(),
+  //         GestureDetector(
+            
+  //           onTap: () {
+              
+  //           },
+    
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -66,10 +91,7 @@ class _CourseScreenState extends State<CourseScreen> {
                       EdgeInsets.only(top: height * 0.06, right: width * 0.50),
                   child: _title('Course', context),
                 ),
-                Padding(
-                  padding:  EdgeInsets.only(top: height * 0.15 ),
-                  child: Container(child: _buildGetAllDataSumbit(context)),
-                )
+                _buildGetAllDataSumbit(context)
               ],
             ),
           )),
