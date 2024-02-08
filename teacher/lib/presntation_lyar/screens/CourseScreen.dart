8ac1147b2618cb89bb1f77ec251c69cadbf0aa18
mody258963 <122531523+mody258963@users.noreply.github.com>
2024-02-08 13:06:06 +0200

    soso

--- teacher/lib/presntation_lyar/screens/CourseScreen.dart
+++ teacher/lib/presntation_lyar/screens/CourseScreen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart ';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:teacher/besnese_logic/get_method/get_method_cubit.dart';
@@ -26,7 +26,49 @@ class _CourseScreenState extends State<CourseScreen> {
         style: TextStyle(color: Colors.white, fontSize: width * 0.12));
   }
 
-  Widget _buildGetAllDataSumbit(BuildContext context) {
+  Widget _buildLectureAllDataTeacher(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return BlocBuilder<GetMethodCubit, GetMethodState>(
+        builder: (context, state) {
+      if (state is LectureOfCourseState) {
+        final allUsersList = state.posts;
+        return SizedBox(
+            height: height * 0.767,
+            width: width * 0.95,
+            child: ListView.builder(
+                itemCount: allUsersList.length,
+                itemBuilder: (context, index) {
+                  final user = allUsersList[index];
+                  return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(7),
+                              color: Colors.yellowAccent),
+                          height: height * 0.20,
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  user.title.toString(),
+                                  style: TextStyle(fontSize: width * 0.10),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    print('==============fododod');
+                                  },
+                                )
+                              ],
+                            ),
+                          )));
+                }));
+      }
+      return Container();
+    });
+  }
+
   PersistentBottomSheetController<void> _buttomsheet() {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
@@ -44,62 +86,6 @@ class _CourseScreenState extends State<CourseScreen> {
           ),
         ),
       );
-    });
-  }
-
-  Widget _buildLectureAllDataTeacher(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    return BlocBuilder<GetMethodCubit, GetMethodState>(
-        builder: (context, state) {
-      if (state is LectureOfCourseState) {
-        final allUsersList = state.posts;
-        return SizedBox(
-          height: height * 0.767,
-          width: width * 0.95,
-          child: ListView.builder(
-              itemCount: allUsersList.length,
-              itemBuilder: (context, index) {
-                final user = allUsersList[index];
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(7),
-                        color: Colors.yellowAccent),
-                    height: height * 0.20,
-                    child: Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            user.title.toString(),
-                            style: TextStyle(fontSize: width * 0.10),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              print('==============fododod');
-                            },
-                          )
-                        ],
-                      ),
-
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(7),
-                          color: Colors.yellowAccent),
-                      height: height * 0.20,
-                      child: Text(user.title.toString()),
-                    ),
-                  ),
-                );
-              }),
-        );
-      }
-
-      return Container();
     });
   }
 
@@ -141,28 +127,6 @@ class _CourseScreenState extends State<CourseScreen> {
       return Container();
     });
   }
-
-  // Widget _Gester(title) {
-  //   double width = MediaQuery.of(context).size.width;
-  //   double height = MediaQuery.of(context).size.height;
-  //   return Container(
-  //     color: Colors.blueAccent,
-  //     height: height * 0.30,
-  //     width: width * 0.80,
-  //     child: Column(
-  //       children: [Text(title),
-  //       Text(),
-  //         GestureDetector(
-
-  //           onTap: () {
-
-  //           },
-
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context) {
